8311d6b3a132656376755f39938639f7d4b161f3
Masanov <masanov3167@gmail.com>
2022-04-24 18:49:23 +0500

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sitebar from './Components/Sitebar/Sitebar';
+import Main from './Components/Main/Main';
+import './main.css';
+function App() {
+	return (
+		<div className='App'>
+      <Sitebar />
+	  <Main />			
+		</div>
+	);
+}
+
+export default App;
